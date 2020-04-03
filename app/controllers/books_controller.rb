5c378870c4e12f39c5514bcab57934eef75b7971
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-04-04 03:26:25 +0900

    [Add]いいね機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
 
 
   def show
+    @books = Book.all
   	@book = Book.find(params[:id])
     @user = current_user
     @book_comment = BookComment.new
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 
 	def favorited_by?(user)
-		favorites.where(user_id: user.id).exist?
+		favorites.where(user_id: user.id).exists?
 	end
 	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
 	#presence trueは空欄の場合を意味する。
